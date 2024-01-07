1852a8d3ba8f4f3feb9b07c389415b43bc54b678
Mustafa BOLEKEN <mustafaboleken@yahoo.com>
2024-01-07 15:01:14 +0300

    Revert "Revert "Add message button for debug purposes""
    This reverts commit a65b86606577bc91ffb6440bbd5b2170e6f29a1f.

--- react/src/pages/AntMedia.js
+++ react/src/pages/AntMedia.js
@@ -1585,7 +1585,7 @@ function AntMedia() {
         webRTCAdaptor.getBroadcastObject(eventStreamId);
       }
       else if (eventType === "MESSAGE_RECEIVED") {
-        if(notificationEvent.senderId === publishStreamId || isFakeeh === true) {
+        if(notificationEvent.senderId === publishStreamId/* || isFakeeh === true*/) {
           return;
         }
         calculate_scroll_height();
