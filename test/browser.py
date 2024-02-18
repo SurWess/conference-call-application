d07676c10666a2936e2cd1b953c0447d8ac35f03
burak-58 <burak@antmedia.io>
2024-02-18 22:37:44 +0300

    add integration tests

--- test/browser.py
+++ test/browser.py
@@ -105,6 +105,9 @@ class Browser:
 
   def get_wait(self):
     return WebDriverWait(self.driver, 15)
+  
+  def get_wait(self, timeout):
+    return WebDriverWait(self.driver, timeout)
 
   def close(self):
     self.driver.close()
